57af29e3a0c2f2176e18cc8480fac7363200b720
vamsi <bellamvamsikrishna3415@gmail.com>
2022-09-19 18:56:06 +0530

    Setup test env for todos with db

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -1,65 +1,18 @@
-const todoList = require("../todo");
+const db = require("../models");
 
-const formatDate = (date) => date.toLocaleDateString("en-CA");
-let date = new Date();
-const today = formatDate(date);
-const yesterday = formatDate(new Date(date.setDate(date.getDate() - 1)));
-const tomorrow = formatDate(new Date(date.setDate(date.getDate() + 2)));
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
 
-const todos = [
-  { title: "Submit assignment", dueDate: yesterday, completed: false },
-  { title: "Pay rent", dueDate: today, completed: true },
-  { title: "Service Vehicle", dueDate: today, completed: false },
-  { title: "File taxes", dueDate: tomorrow, completed: false },
-  { title: "Pay electric bill", dueDate: tomorrow, completed: false },
-];
-
-const addTestData = (add, todos) => todos.forEach(add);
-
-/* eslint-disable no-undef */
-describe("Todo List Test Suite", () => {
-  test("Should add a new todo", () => {
-    const { all, add } = todoList();
-    expect(all.length).toEqual(0);
-    add({
-      title: "A test item",
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
       completed: false,
-      dueDate: today,
+      dueDate: new Date(),
     });
-    expect(all.length).toEqual(1);
-  });
-
-  test("Should mark a todo as complete", () => {
-    const { all, markAsComplete, add } = todoList();
-    add({
-      title: "A test item",
-      completed: false,
-      dueDate: today,
-    });
-    expect(all.length).toEqual(1);
-    expect(all[0].completed).toEqual(false);
-    markAsComplete(0);
-    expect(all[0].completed).toEqual(true);
-  });
-
-  test("Should list the overdue todos", () => {
-    const { all, overdue, add } = todoList();
-    addTestData(add, todos);
-    expect(all.length).toEqual(5);
-    expect(overdue().length).toEqual(1);
-  });
-
-  test("Should list the due today todos", () => {
-    const { all, dueToday, add } = todoList();
-    addTestData(add, todos);
-    expect(all.length).toEqual(5);
-    expect(dueToday().length).toEqual(2);
-  });
-
-  test("Should list the due later todos", () => {
-    const { all, dueLater, add } = todoList();
-    addTestData(add, todos);
-    expect(all.length).toEqual(5);
-    expect(dueLater().length).toEqual(2);
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
--- models/todo.js
+++ models/todo.js
@@ -7,6 +7,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static async addTask(params) {
+      return await Todo.create(params);
+    }
+
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+    }
+
     // eslint-disable-next-line no-unused-vars
     static associate(models) {
       // define association here
